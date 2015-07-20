3f9d712cc1c475b07e02de90e3b8ef9369111f51
Tate, Hongliang Tian <tatetian@gmail.com>
2015-07-20 15:21:02 +0800

    Add fake file system

---
+++ src/fs.js
@@ -0,0 +1,128 @@
+/*
+ *
+ **/
+
+var INode = function(name) {
+    this.name = name;
+    this.parent = null;
+};
+
+INode.prototype.toPath = function() {
+    var path = '';
+    if (this.parent) path += this.parent.toPath();
+    path += this.name;
+    if (this.isDirectory()) path += '/';
+    return path;
+};
+
+INode.prototype.addChild = function(child) {
+    if (!this.children) this.children = [];
+    child.parent = this;
+    this.children.push(child);
+};
+
+INode.prototype.isDirectory = function() {
+    return !!this.children;
+};
+
+INode.loadFromJson = function(json){
+    var inode = new INode(json.name);
+
+    var children = json.nodes;
+    if (!children) return inode;
+
+    var numNodes = json.nodes.length;
+    for (var ni = 0; ni < numNodes; ni++) {
+        var childNode = INode.loadFromJson(json.nodes[ni]);
+        inode.addChild(childNode);
+    }
+    return inode;
+};
+
+INode.getByPath = function(path) {
+    var parts = path.split('/');
+
+};
+
+
+var rootNode = INode.loadFromJson(JSON.parse(
+                    require("fs").readFileSync('./fs.json', 'utf8')));
+var cwdNode = rootNode;
+
+function getINodeByPath(path) {
+    console.log('get inode of path ' + path);
+
+    path = path.trim();
+    if (!path || path === '') return null;
+
+    // only accept relative paths or absolute paths begin with '~'
+    var pathParts = path.split('/');
+    // absolute path doesn't make sense for our purpose
+    if (pathParts[0] === '')
+        return null;
+
+    // if not start with '~', then the path is relative to cwd
+    var resNode = pathParts[0] !== '~' ? cwdNode : rootNode;
+    for (var pi = (pathParts[0] !== '~' ? 0 : 1);
+            pi < pathParts.length; pi++) {
+        var partName = pathParts[pi];
+
+        if (partName === '' || partName === '.') {
+            if (!resNode.isDirectory()) return null;
+            else continue;
+        }
+
+        if (partName === '..') {
+            resNode = resNode.parent;
+            if (resNode === null) return null;
+        }
+
+        var children = resNode.children;
+        if (!children) return null;
+        var numChildren = children.length;
+        for (var ci = 0; ci < numChildren; ci++) {
+            var node = children[ci];
+            // found a node that matches
+            if (partName === node.name) {
+                resNode = node;
+                break;
+            }
+        }
+
+        // no matched node found
+        if (ci === numChildren) return null;
+    }
+    return resNode;
+}
+
+var fs = {};
+
+fs.ls = function(dir) {
+    var inode = getINodeByPath(dir);
+    if (!inode) return {err: 'No such file or directory'};
+
+    var list = [];
+    if (!inode.isDirectory())
+        list.push(inode.name);
+    else {
+        var nodes = inode.children;
+        var numNodes = nodes.length;
+        for (var ni = 0; ni < numNodes; ni++)
+            list.push(nodes[ni].name);
+    }
+    return {res: list};
+};
+
+var cwdNode = rootNode;
+fs.cwd = function(dir) {
+    if (dir) {
+        var inode = getINodeByPath(dir);
+        if (!inode) return {err: 'No such file or directory'};
+        if (!inode.isDirectory()) return {err: 'Not a directory'};
+
+        cwdNode = inode;
+    }
+    return {res: cwdNode.toPath()};
+};
+
+module.exports = fs;
